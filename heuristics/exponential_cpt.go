760c1b5d1d151f0d2a6fe03d17898ff4de566cae
Patrick-Ranjit D. Madsen <pamad05+github@gmail.com>
2014-05-07 22:21:23 +0200

    two heuristics utilising the interface Model

---
+++ heuristics/exponential_cpt.go
@@ -0,0 +1,75 @@
+package statedb
+
+import (
+	"errors"
+	"github.com/paddie/statedb/monitor"
+	"time"
+)
+
+type Exponential struct {
+	Mean   float64
+	Lambda float64
+	Bid    float64
+	Stat   *Stat
+	Price  float64
+}
+
+func NewExponential() *Exponential {
+	return &Exponential{}
+}
+
+func (e *Exponential) Name() string {
+	return "ExponentialDistribution"
+}
+
+func (e *Exponential) take(t, t_p float64) float64 {
+
+	return 0.0, errors.New("Something")
+}
+
+func (e *Exponential) skip(t, t_p float64) float64 {
+	return 0.0, errors.New("Something")
+}
+
+func (e *Exponential) t(w, t_p float64) float64 {
+	return 0.0
+}
+
+func (e *Exponential) Train(trace *monitor.Trace, bid float64) error {
+	e.Mean, _, _, _, _ = trace.AliveStats(bid)
+	e.Lambda = 1.0 / e.Mean
+	e.Price = trace.Latest.SpotPrice
+	return nil
+}
+
+func (e *Exponential) Checkpoint(w, t_p time.Duration) (bool, error) {
+
+	// to get stats a checkpoint is required
+	// - stats are only sent when a checkpoint is completed
+	if e.stat == nil {
+		return true, nil
+	}
+
+	take, err := e.take(w, t_p)
+	if err != nil {
+		return false, err
+	}
+
+	skip, err := e.skip(w, t_p)
+	if err != nil {
+		return false, err
+	}
+
+	return take < skip, nil
+}
+
+func (e *Exponential) StatUpdate(s *Stat) error {
+	e.Stat = s
+	return nil
+}
+
+func (e *Exponential) PriceUpdate(p float64) error {
+	e.Price = p
+
+	return nil
+}
---
+++ heuristics/rising_cpt.go
@@ -0,0 +1,53 @@
+package statedb
+
+import (
+	"errors"
+	"github.com/paddie/statedb/monitor"
+)
+
+type RisingEdge struct {
+	price float64
+	risen bool
+}
+
+func NewRisingEdge() *RisingEdge {
+	return
+}
+
+func (r *RisingEdge) Name() string {
+	return "RisingEdge"
+}
+
+func (r *RisingEdge) Train(trace *monitor.Trace, _ float64) error {
+
+	r.price = trace.Latest.SpotPrice
+
+	return nil
+}
+
+func (r *RisingEdge) StatUpdate(_ *Stat) error {
+	return nil
+}
+
+func (r *RisingEdge) PriceUpdate(p float64) error {
+
+	if r.price < p {
+		r.risen = true
+	}
+	r.price = p
+
+	return nil
+}
+
+func (r *RisingEdge) Checkpoint(_, _ time.Duration) (bool, error) {
+	if r.risen {
+		r.risen = false
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (r *RisingEdge) Quit() error {
+	return nil
+}
