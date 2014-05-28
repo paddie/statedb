987fba7fcd9d2ad46e15b345d784a87e7e628ba0
Patrick-Ranjit D. Madsen <pamad05+github@gmail.com>
2014-05-29 01:04:45 +0200

    avg buffer now has time.Duration units for averages

--- avgbuffer/avgtime.go
+++ avgbuffer/avgtime.go
@@ -1,6 +1,6 @@
 package avgbuffer
 
-// The AVGFloat64 is a circular buffer that will never grow
+// The AVGBuffer is a circular buffer that will never grow
 // bigger then the provided size.
 //
 // Every time Upsert(val) is called on the buffer, we do one of two things depending on the buffer being full or not:
@@ -9,26 +9,28 @@ package avgbuffer
 // 2. Not full: Insert the value, update the sum and the average
 import (
 	"fmt"
+	"time"
 )
 
-type AVGFloat64 struct {
-	buff []float64
+type AVGDuration struct {
+	buff []time.Duration
 	i    int
-	sum  float64
-	avg  float64
+	sum  time.Duration // sum of all intervals
+	avg  float64       // average
+	tavg time.Duration // rounded ns value
 }
 
-func NewAVGFloat64(size int) *AVGFloat64 {
-	return &AVGFloat64{
-		buff: make([]float64, 0, size),
+func NewAVGDuration(size int) *AVGDuration {
+	return &AVGDuration{
+		buff: make([]time.Duration, 0, size),
 	}
 }
 
-func (b *AVGFloat64) String() string {
+func (b *AVGDuration) String() string {
 	return fmt.Sprintf("avg=%.2f buff=%v (i=%d, size=%d)", b.avg, b.buff, b.i, len(b.buff))
 }
 
-func (b *AVGFloat64) Upsert(val float64) float64 {
+func (b *AVGDuration) Upsert(val time.Duration) time.Duration {
 
 	if len(b.buff) < cap(b.buff) {
 		// append until we reach 10
@@ -42,20 +44,22 @@ func (b *AVGFloat64) Upsert(val float64) float64 {
 	// update sum
 	b.sum += val
 	// update average
-	b.avg = b.sum / float64(len(b.buff))
+	// - round to nearest ns
+	b.avg = float64(b.sum) / float64(len(b.buff))
+	b.tavg = time.Duration(b.avg)
 
 	// update pointer
-	// - use capasity for pointer update
+	// - use capacity for pointer update
 	if b.i == cap(b.buff)-1 {
 		b.i = 0
 	} else {
 		b.i += 1
 	}
 
-	return b.avg
+	return time.Duration(b.avg)
 }
 
-func (b *AVGFloat64) Get(i int) (float64, error) {
+func (b *AVGDuration) Get(i int) (time.Duration, error) {
 	if i < 0 || i > len(b.buff)-1 {
 		return -1, fmt.Errorf("buff: [0;%d] invalid i=%d", len(b.buff)-1, i)
 	}
@@ -63,7 +67,7 @@ func (b *AVGFloat64) Get(i int) (float64, error) {
 	return b.buff[i], nil
 }
 
-func (b *AVGFloat64) MostRecent() float64 {
+func (b *AVGDuration) MostRecent() time.Duration {
 	if len(b.buff) == 0 {
 		return -1
 	}
@@ -75,14 +79,18 @@ func (b *AVGFloat64) MostRecent() float64 {
 	return b.buff[b.i-1]
 }
 
-func (b *AVGFloat64) AVG() float64 {
+func (b *AVGDuration) AVG() float64 {
 	return b.avg
 }
 
-func (b *AVGFloat64) Cap() int {
+func (b *AVGDuration) TAVG() time.Duration {
+	return b.tavg
+}
+
+func (b *AVGDuration) Cap() int {
 	return cap(b.buff)
 }
 
-func (b *AVGFloat64) Size() int {
+func (b *AVGDuration) Size() int {
 	return len(b.buff)
 }
